bd3bd947a59d84234d7318ecb9d9914f61e0182c
vali281 <demonxx2004@gmail.com>
2026-02-02 15:02:38 +0530

    Variables and lietrals

---
+++ prog3/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int num3 = 8;
+        int sum = num1 + num2 + num3;
+        System.out.println("The sum is: " + sum);
+    }
+}
