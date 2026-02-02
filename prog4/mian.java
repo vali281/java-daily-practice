bdc2c4f463291c7f7e0a299b63d0689a52aef141
vali281 <demonxx2004@gmail.com>
2026-02-02 15:09:23 +0530

    Variables and lietrals

---
+++ prog4/mian.java
@@ -0,0 +1,11 @@
+public class mian {
+    public static void main(String[] args) {
+        // Literals are the values that can be stored inside a variable
+        int age = 12;
+        long number = 5666666666666l;
+        char ch = 'A';
+        float f1 = 5.5f;
+        double d1 = 6.7d;
+
+    }
+}
